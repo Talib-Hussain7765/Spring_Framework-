35f81e6c42feaf7017fc2fd7f071a7800f6df873
Talib-Hussain7765 <97027978+Talib-Hussain7765@users.noreply.github.com>
2022-03-07 21:05:12 +0530

    Update Tyre.java

--- Telusko5/src/main/java/com/navin/Telusko/Tyre.java
+++ Telusko5/src/main/java/com/navin/Telusko/Tyre.java
@@ -3,12 +3,13 @@ package com.navin.Telusko;
 public class Tyre
 {
 	private String brand;
-	
+	private int height;
 	
 
-	public Tyre(String brand) {
+	public Tyre(String brand,int height) {
 		super();
 		this.brand = brand;
+		this.height= height;
 	}
 
 	public String getBrand() {
@@ -17,6 +18,14 @@ public class Tyre
 
 	public void setBrand(String brand) {
 		this.brand = brand;
+	}
+	
+	public int getHeight(){
+		return height;
+	}
+	
+	public void setHeight(int height){
+		this.height=height;
 	}
 
 	@Override
